9fc84f8678017468af6d026ad3102d02dd9955d4
Mert ÜNAL <mertunal1998@gmail.com>
2025-11-09 22:27:47 +0300

    vsco link added

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -12,6 +12,7 @@ const year = new Date().getFullYear();
                 <div className="border-t-1 border-gray-500"></div>
                  <div className="flex flex-row justify-center gap-12">
                     <LogoAndLink link="https://letterboxd.com/troemanes/" name="LetterBoxd" img="/letterboxd.png"/>
+                    <LogoAndLink link="https://vsco.co/troemanes/gallery" name="VSCO" img="/vsco.png"/>
 
                 </div>                
                 <p className="text-center py-6">Bu blog <span className="underline">{user.name}</span> tarafından yapılmıştır.<br></br>  © {new Date().getFullYear()} Tüm hakları saklıdır.</p>
@@ -24,7 +25,7 @@ const year = new Date().getFullYear();
 function LogoAndLink ({img,name,link}){
     return(<div className="flex flex-col   hover:text-orange-300 p-5 mt-6 text-center items-center align-center justify-center ">
         <a href={link} target="_blank"> 
-         <img   className="object-contain size-10 w-full" src={img}/>
+         <img   className="object-contain size-10 w-full rounded-full" src={img}/>
          <p className="py-3 text-center text-sm  font-medium">{name}</p> 
          </a>
     </div>)
--- src/Components/Navigation.jsx
+++ src/Components/Navigation.jsx
@@ -15,12 +15,18 @@ export default function Navigation(){
     ? "text-orange-400 font-semibold transition-all md:text-xl duration-500"
     : "hover:text-orange-300 transition-all md:text-lg duration-500";
     return(
-        <div className="navbar px-5 md:px-12 md:py-6 m-0 border-b-1  border-t-1 border-b-gray-500 fixed z-2 bg-[var(--accent1)] top-0 left-0 border-t-gray-500 justify-between ">
+   
+  
+   <div className="navbar px-5    md:px-12 md:py-6 m-0 border-b-1  border-t-1 border-b-gray-500 fixed z-2 bg-[var(--accent1)] top-0 left-0 border-t-gray-500 justify-between ">
 
 
  
   
+  
+
  <div className="md:hidden">
+
+
     <div className="dropdown ">
       <div tabIndex={0}  role="button">
         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round"  strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h12" /> </svg>
@@ -53,6 +59,6 @@ export default function Navigation(){
     <ToggleTheme change={change} theme={theme}/>
     <a className="text-2xl text-orange-300 font-semibold"  /* target="_blank" href="https://my-portfolyo-taupe.vercel.app/" */ >{user.name}</a>
   </div>
-</div>
+ </div>
             )
 }
